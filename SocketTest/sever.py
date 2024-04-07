71a7963391389e5f9bc769726315d202e8677213
Do Nguyen Hoang Tuan <126333262+MrDemoa@users.noreply.github.com>
2024-04-07 21:14:13 +0700

    update songlist

--- SocketTest/sever.py
+++ SocketTest/sever.py
@@ -42,7 +42,7 @@ def send_audio(filename):
 filename = client.recv(1024).decode()
 print("FILENAME FROM SEVER: " + filename)
 
-project_directory = "C:\\Users\\Hoang Tuan\Documents\\GitHub\\HarmonyHub\\SocketTest\\resource\\"
+project_directory = "C:\\Users\\Hoang Tuan\Documents\\GitHub\\HarmonyHub\\SocketTest\\resource\\SongList"
 audio_path = os.path.join(project_directory, filename)
 
 # Send audio data to the client
